455c7d018eb6d0d50f2173a764e2fb0f15a3aa47
yuriq <yuriqbarbosa@gmail.com>
2024-09-30 17:12:57 -0300

    commitei

--- src/components/content.vue
+++ src/components/content.vue
@@ -8,19 +8,14 @@ function filter() {
 
 <template>
     <div class="flex">
-        <aside class="fixed left-0 top-0 w-28 h-full bg-grey-800 text-center">
+        <aside class="fixed left-0 top-0 w-28 h-full bg-gray-800 text-center">
             <h1 class="text-white">teste</h1>
         </aside>
         <main class="flex ml-28">
             <div class="max-w-screen-sm w-auto ml-20 mt-14 p-2 bg-white rounded-lg" id="container">
                 <span> svg </span>
-                <input class="w-80 size-7 bg-purple p-2 outline-none" type="text" placeholder="Pesquisa"/>
+                <input class="w-80 size-7 bg-purple p-2" type="text" placeholder="Pesquisa"/>
                 <button class="pl-1" @click="filter">filtro</button>
-            </div>
-            <div class="absolute top-2/4 left-2/4 p-8 bg-white">
-                <ul>
-                    teste
-                </ul>
             </div>
         </main>
     </div>  
